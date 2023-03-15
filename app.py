3dbf85438e0b0c561e46ff5a4d222cd708e1d2c7
aminachaihab <aminachaihab06@gmail.com>
2023-03-16 00:52:43 +0100

    commit1

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-def index():
-    return "hello , world !"
+# Demander à l'utilisateur d'entrer le nombre et la puissance
+nombre = float(input("Entrez un nombre : "))
+puissance = int(input("Entrez la puissance : "))
 
+# Calculer la puissance
+resultat = nombre ** puissance
+
+# Afficher le résultat
+print("{} à la puissance {} est égal à {}".format(nombre, puissance, resultat))
 
 
 
