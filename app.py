b2918a6e4f32438efbe010c477e689973cee26c3
aminachaihab <127900856+aminachaihab@users.noreply.github.com>
2023-03-16 01:36:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-# Demander à l'utilisateur d'entrer le nombre et la puissance
-nombre = float(input("Entrez un nombre : "))
-puissance = int(input("Entrez la puissance : "))
-
-# Calculer la puissance
-resultat = nombre ** puissance
-
-# Afficher le résultat
-print("{} à la puissance {} est égal à {}".format(nombre, puissance, resultat))
-
-
-
+def calculer_puissance(nombre, puissance):
+    # Calculer la puissance
+    resultat = nombre ** puissance
+    return resultat
