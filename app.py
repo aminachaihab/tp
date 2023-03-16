e8ed0b5f3ba4e8ec28422f06645e4a5afa53ffde
aminachaihab <127900856+aminachaihab@users.noreply.github.com>
2023-03-16 01:48:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 def calculer_puissance(nombre, puissance):
-    # Calculer la puissance
+    """
+    Cette fonction calcule la puissance d'un nombre donné.
+
+    Args:
+        nombre (float): Le nombre à calculer.
+        puissance (int): La puissance à laquelle élever le nombre.
+
+    Returns:
+        float: Le résultat du calcul de la puissance.
+    """
     resultat = nombre ** puissance
     return resultat
