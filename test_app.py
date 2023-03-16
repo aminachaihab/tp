413c1da1dff3e0b849079eb1f419c801d0c17695
aminachaihab <127900856+aminachaihab@users.noreply.github.com>
2023-03-16 01:03:03 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,13 +2,9 @@
 import pytest
 from app import calculer_puissance
 
-def test_puissance_entier_positif():
+def test_calculer_puissance():
     assert calculer_puissance(2, 3) == 8
-
-def test_puissance_entier_negatif():
-    assert calculer_puissance(-2, 3) == -8
-
-def test_puissance_flottant():
-    assert pytest.approx(calculer_puissance(2.5, 2), 0.01) == 6.25
+    assert calculer_puissance(5, 0) == 1
+    assert calculer_puissance(3, 4) == 81
 
 
